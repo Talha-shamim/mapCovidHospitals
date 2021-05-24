76efce959f132500e248e88747637c58d5ab6cc2
Talha-shamim <66217619+Talha-shamim@users.noreply.github.com>
2021-05-24 23:37:35 +0530

    Update basicmap.js

--- src/components/ReactLeaflet/basicmap.js
+++ src/components/ReactLeaflet/basicmap.js
@@ -92,6 +92,7 @@ const BasicMap = () => {
                 );
               })}
 
+              <Marker position={[LAT, LNG]} />
               <Marker position={[userLatitude, userLongitude]} />
             </MapContainer>
           </div>
